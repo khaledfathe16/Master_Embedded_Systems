e9cf4a770dc5af6d8f217af6d037ee710ca64db8
Khaled Fathy <khaled.fathe16@gmail.com>
2023-09-12 20:32:26 +0300

    .

--- lol.c
+++ lol.c
@@ -3,11 +3,19 @@
 //#include <stdlib.h>
 //#include <conio.h>
 
+
 int main()
 {
-     
+
+	int a = 5 , b = 0 , c;
 	
+	c = (a>b)?a:b;
+
+    printf("Maximum number = %d" , c);
+
 	
+ 
 	
 	return 0;
 }
+
