246ab3d6f4d5bcc4279bd8887ea63b6625a8915a
Khaled Fathy <khaled.fathe16@gmail.com>
2024-01-18 16:15:11 +0200

    .

--- lol.c
+++ lol.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 
-// int lol (void);
  int main() {
  
 
- int fun(void)
-{
-int a = 5;
-return 0;
-}
-
- 
-printf("%d",fun());
 
     return 0;
 }
 
 
-/*int lol (void)
-{
-	
-	return fun();
-}*/
